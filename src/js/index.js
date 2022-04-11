2acb40a83682e643a092b47e2fab7ec597415dc5
Anton <anton_snov@mail.ru>
2022-04-11 16:33:59 +0300

    day 4

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,9 @@ const book = document.querySelector('.book');
 const add = document.querySelector('.add');
 const addBtns = document.querySelectorAll('.header__btn-add, .library__btn-add');
 const backBtns = document.querySelectorAll('.header__btn_back');
-
+const btnSearch = document.querySelectorAll('.header__btn_search');
+const search = document.querySelector('.search');
+const btnHeaderAdd = document.querySelector('.header__btn-add');
 
 const router = new Navigo('/', {
     hash: true,
@@ -25,6 +27,7 @@ router.on({
     'book': () => {
         closeAllPage();
         book.classList.remove('hidden');
+        search.classList.remove('search_active');
     },
     'add': () => {
         closeAllPage();
@@ -38,9 +41,27 @@ addBtns.forEach(btn => {
     });
 });
 
-//  дз
 backBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         router.navigate('/');
+    });
+});
+
+
+
+const closeSearch = ({target}) => {
+    if (target.closest('.search, .header__btn_search')) {
+        return;
+    }
+    search.classList.remove('search_active');
+    btnHeaderAdd.classList.remove('hidden');
+    document.body.removeEventListener('click', closeSearch);
+};
+
+btnSearch.forEach(btn => {
+    btn.addEventListener('click', () => {
+        search.classList.add('search_active');
+        btnHeaderAdd.classList.add('hidden');
+        document.body.addEventListener('click', closeSearch);
     });
 });
